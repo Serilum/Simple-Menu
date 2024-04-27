041cb4d2e49d76447f55bd58ac91a1349c7c6303
Rick South <rick@natamus.com>
2024-04-28 01:25:37 +0200

    Simple Menu source code for 1.20.5.

---
+++ Common/src/main/java/com/natamus/simplemenu/util/Reference.java
@@ -0,0 +1,8 @@
+package com.natamus.simplemenu.util;
+
+public class Reference {
+	public static final String MOD_ID = "simplemenu";
+	public static final String NAME = "Simple Menu";
+	public static final String VERSION = "1.1";
+	public static final String ACCEPTED_VERSIONS = "[1.20.5]";
+}
