b4b1508b126d8f56ca6a7b57f9eace42ed8c3498
Rick South <rick@natamus.com>
2024-08-15 15:15:17 +0200

    (v1.5) (Forge) Fixed the mod not loading correctly due to a mixin conflict.

--- Common/src/main/java/com/natamus/simplemenu/util/Reference.java
+++ Common/src/main/java/com/natamus/simplemenu/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.simplemenu.util;
 public class Reference {
 	public static final String MOD_ID = "simplemenu";
 	public static final String NAME = "Simple Menu";
-	public static final String VERSION = "1.4";
+	public static final String VERSION = "1.5";
 	public static final String ACCEPTED_VERSIONS = "[1.20.6]";
 }
