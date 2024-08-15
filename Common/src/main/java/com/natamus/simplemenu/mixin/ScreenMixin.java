22fa64c5a5dd4ef9a7d6093d4094a95373aef35f
Rick South <rick@natamus.com>
2024-08-15 15:15:13 +0200

    (v1.5) (Forge) Fixed the mod not loading correctly due to a mixin conflict.

--- Common/src/main/java/com/natamus/simplemenu/mixin/ScreenMixin.java
+++ Common/src/main/java/com/natamus/simplemenu/mixin/ScreenMixin.java
@@ -1,5 +1,6 @@
 package com.natamus.simplemenu.mixin;
 
+import com.natamus.collective.functions.ScreenFunctions;
 import com.natamus.simplemenu.config.ConfigHandler;
 import com.natamus.simplemenu.data.Buttons;
 import com.natamus.simplemenu.data.Constants;
@@ -12,15 +13,12 @@ import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.TitleScreen;
 import net.minecraft.network.chat.Component;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(value = Screen.class, priority = 1001)
 public abstract class ScreenMixin {
-	@Shadow protected abstract <T extends GuiEventListener & Renderable & NarratableEntry> T addRenderableWidget(T $$0);
-
 	@Inject(method = "addRenderableWidget(Lnet/minecraft/client/gui/components/events/GuiEventListener;)Lnet/minecraft/client/gui/components/events/GuiEventListener;", at = @At(value = "HEAD"))
 	protected <T extends GuiEventListener & Renderable & NarratableEntry> void addRenderableWidget(T renderableWidget, CallbackInfoReturnable<T> cir) {
 		Screen screen = (Screen)(Object)this;
@@ -36,7 +34,7 @@ public abstract class ScreenMixin {
 				}
 
 				if (ConfigHandler.enableServerPromoButton) {
-					Buttons.serverPromoButton = (Button)addRenderableWidget(Button.builder(Component.literal(ConfigHandler.serverPromoButtonTextDefault), (f) -> {
+					Buttons.serverPromoButton = (Button) ScreenFunctions.addRenderableWidget(screen, Button.builder(Component.literal(ConfigHandler.serverPromoButtonTextDefault), (f) -> {
 						Util.openUrl(ConfigHandler.serverPromoButtonClickURL);
 					}).bounds(button.getX(), button.getY(), button.getWidth(), button.getHeight()).build());
 				}
